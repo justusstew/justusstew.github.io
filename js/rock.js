0d605681c6f800869ede8cc907790e526532d154
justusstew <js10.stewart@gmail.com>
2015-10-12 21:09:03 -0500

    more game logic

--- js/rock.js
+++ js/rock.js
@@ -1,2 +1,29 @@
 var user_choice = '';
 var options = ['rock','paper','scissors'];
+var justus_choice = '';
+var user_score = 0;
+var justus_score = 0;
+
+$('#user_score').append(user_score);
+$('#justus_score').append(justus_score);
+
+function evaluate_choices(choice1,choice2){
+  if(choice1 == choice2){
+    return 'Tie!';
+  }elseif(choice1 == 'rock' && choice2 == 'paper'){
+
+  }
+}
+
+$('#rock').on('click',function(){
+  user_choice = 'rock';
+  justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+});
+$('#paper').on('click',function(){
+  user_choice = 'paper';
+  justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+});
+$('#scissors').on('click',function(){
+  user_choice = 'scissors';
+  justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+});
