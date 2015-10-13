4eaa4659acd74c590c469ff10e6234f91aab8f1f
justusstew <js10.stewart@gmail.com>
2015-10-12 21:33:03 -0500

    fix

--- js/rock.js
+++ js/rock.js
@@ -11,44 +11,79 @@ function evaluate_choices(choice1,choice2){
   if(choice1 == choice2){
     $('#game_message').empty();
     $('#game_message').append('Tie');
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }
   else if(choice1 == 'rock' && choice2 == 'paper'){
     $('#game_message').empty();
     $('#game_message').append('Justus Wins!');
     justus_score = parseInt($('#justus_score').val()) + 1;
     $('#justus_score').val(justus_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }else if(choice1 == 'paper' && choice2 == 'scissors'){
     $('#game_message').empty();
     $('#game_message').append('Justus Wins!');
     justus_score = parseInt($('#justus_score').val()) + 1;
     $('#justus_score').val(justus_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }else if(choice1 == 'scissors' && choice2 == 'rock'){
     $('#game_message').empty();
     $('#game_message').append('Justus Wins!');
     justus_score = parseInt($('#justus_score').val()) + 1;
     $('#justus_score').val(justus_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }else if(choice1 == 'paper' && choice2 == 'rock'){
     $('#game_message').empty();
     $('#game_message').append('You Win!');
     user_score = parseInt($('#user_score').val()) + 1;
     $('#user_score').val(user_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }else if(choice1 == 'scissors' && choice2 == 'paper'){
     $('#game_message').empty();
     $('#game_message').append('You Win!');
     user_score = parseInt($('#user_score').val()) + 1;
     $('#user_score').val(user_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }else if(choice1 == 'rock' && choice2 == 'scissors'){
     $('#game_message').empty();
     $('#game_message').append('You Win!');
     user_score = parseInt($('#user_score').val()) + 1;
     $('#user_score').val(user_score);
-    setTimeout(function(){ $('#game_message').empty(); }, 1500);
+    $('#justus_choice').empty();
+    $('#user_choice').empty();
+    $('#justus_choice').append(justus_choice);
+    $('#user_choice').append(user_choice);
+    setTimeout(function(){ $('#game_message').empty();$('#justus_choice').empty();
+    $('#user_choice').empty(); }, 1500);
   }
 }
 
