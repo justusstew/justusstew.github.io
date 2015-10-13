5ece75a589073bce58dde73ec2bf04dff36620b4
justusstew <js10.stewart@gmail.com>
2015-10-12 21:12:02 -0500

    fix

--- js/rock.js
+++ js/rock.js
@@ -10,20 +10,24 @@ $('#justus_score').append(justus_score);
 function evaluate_choices(choice1,choice2){
   if(choice1 == choice2){
     return 'Tie!';
-  }elseif(choice1 == 'rock' && choice2 == 'paper'){
-
+  }
+  else if(choice1 == 'rock' && choice2 == 'paper'){
+    return 'Justus wins!';
   }
 }
 
 $('#rock').on('click',function(){
   user_choice = 'rock';
   justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+  evaluate_choices(user_choice,justus_choice);
 });
 $('#paper').on('click',function(){
   user_choice = 'paper';
   justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+  evaluate_choices(user_choice,justus_choice);
 });
 $('#scissors').on('click',function(){
   user_choice = 'scissors';
   justus_choice = options[Math.floor(Math.random() * 3) + 1].toString();
+  evaluate_choices(user_choice,justus_choice);
 });
